18fe26a975d81d819f58626a13ccc253c6061311
Hamlet <hamlet94@live.ru>
2020-08-06 16:19:05 +0400

    added passport and some vue js packages

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{any?}/{fist?}/{second?}/{forth?}', function () {
+    return view('layouts.app');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Auth::routes();
+//Route::get('/home', 'HomeController@index')->name('home');
